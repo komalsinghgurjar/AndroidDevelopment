496c85d104823ee83034bc963761c3ad7800dc99
Komal Singh <komalsin9h@gmail.com>
2024-03-15 15:52:29 +0530

    improvements

---
+++ ProjectTemplets/ComposeAdmob/DemoApp/app/src/main/java/gurjar/singh/komal/demoapp/MainActivity.kt
@@ -0,0 +1,210 @@
+package gurjar.singh.komal.demoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import gurjar.singh.komal.demoapp.ui.theme.DemoAppTheme
+
+// Util imports
+import java.util.concurrent.atomic.AtomicBoolean
+import android.util.Log
+import android.content.Context
+import androidx.compose.ui.viewinterop.AndroidView
+
+
+// UMP consent Imports
+import gurjar.singh.komal.demoapp.ads.GoogleMobileAdsConsentManager
+
+
+// Admob fixed banner specific imports
+//import com.google.android.gms.ads.MobileAds
+//import com.google.android.gms.ads.AdRequest
+//import com.google.android.gms.ads.AdSize
+//import com.google.android.gms.ads.AdView
+
+// Admob adaptive banner specific imports
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import androidx.window.layout.WindowMetricsCalculator
+import androidx.compose.foundation.layout.fillMaxWidth
+
+
+// remove in production
+import android.widget.Toast
+
+
+class MainActivity : ComponentActivity() {
+	// Core properties //
+	private lateinit var context: Context
+	companion object {
+	private const val TAG = "MainActivity"
+	}
+
+    // UMP properties //
+    private lateinit var googleMobileAdsConsentManager: GoogleMobileAdsConsentManager
+
+ // Admob properties //
+ private val isMobileAdsInitializeCalled = AtomicBoolean(false)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        setContent {
+            DemoAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                   // AdmobFixedBannerAd("ca-app-pub-3940256099942544/9214589741")
+                   AdmobAdaptiveBannerAd("ca-app-pub-3940256099942544/6300978111")
+                   AdmobAdaptiveBannerAd("ca-app-pub-3940256099942544/9214589741")
+                }
+            }
+        }
+        
+      // init properties
+      context = applicationContext
+        
+      // init methods
+      initUmpConsent()
+
+    }
+    
+    
+    
+    // UMP methods //
+ private fun initUmpConsent() {
+    googleMobileAdsConsentManager = GoogleMobileAdsConsentManager.getInstance(applicationContext)
+    googleMobileAdsConsentManager.gatherConsent(this) { error ->
+      if (error != null) {
+        // Consent not obtained in current session.
+        Log.d(TAG, "${error.errorCode}: ${error.message}")
+        //print ("${error.errorCode}: ${error.message}")
+      }
+      else
+     {
+     	//print ("error in consent")
+     }
+
+      if (googleMobileAdsConsentManager.canRequestAds) {
+      	// consent gathered
+        //print ("consent gathered")
+        initializeMobileAdsSdk()
+      }
+
+      if (googleMobileAdsConsentManager.isPrivacyOptionsRequired) {
+        // Regenerate the options menu to include a privacy setting.
+        //invalidateOptionsMenu()
+        includePrivacySetting()
+      }
+    }
+
+    // This attempts to load ads using consent obtained in the previous session.
+    if (googleMobileAdsConsentManager.canRequestAds) {
+      initializeMobileAdsSdk()
+    }
+
+  }
+
+private fun includePrivacySetting() {
+	// TODO: show privacy options button
+    return
+}
+
+private fun initializeMobileAdsSdk() {
+    if (isMobileAdsInitializeCalled.getAndSet(true)) {
+      return
+    }
+
+    // Initi Admob
+    initAdmob()
+}
+
+// Admob methods //
+private fun initAdmob() {
+    // Initialize the Mobile Ads SDK.
+    MobileAds.initialize(this) {}
+    
+    // TODO: Show ads after
+}
+
+  
+  
+  
+// Testing, remove in production//
+//private fun print(message: String) {
+//    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+//}
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+
+// Admob Adaptive Banner Ad
+@Composable
+fun AdmobAdaptiveBannerAd(bannerAdUnitId: String, modifier: Modifier = Modifier) {
+    AndroidView(
+        modifier = modifier.fillMaxWidth(),
+        factory = { context ->
+            val windowMetrics: androidx.window.layout.WindowMetrics = WindowMetricsCalculator.getOrCreate().computeCurrentWindowMetrics(context)
+            val bounds = windowMetrics.bounds
+
+            var adWidthPixels: Float = context.resources.displayMetrics.widthPixels.toFloat()
+
+            if (adWidthPixels == 0f) {
+                adWidthPixels = bounds.width().toFloat()
+            }
+
+            val density: Float = context.resources.displayMetrics.density
+            val adWidth = (adWidthPixels / density).toInt()
+
+            AdView(context).apply {
+                setAdSize(AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(context, adWidth))
+                adUnitId = bannerAdUnitId
+                loadAd(AdRequest.Builder().build())
+            }
+        }
+    )
+}
+
+// Admob Fixed Banner Ad
+//@Composable
+//fun AdmobFixedBannerAd(bannerAdUnitId: String, modifier: Modifier = Modifier) {
+//    AndroidView(
+//        modifier = modifier,
+//        factory = { context ->
+//            AdView(context).apply {
+//                setAdSize(AdSize.BANNER)
+//                adUnitId = bannerAdUnitId
+//                loadAd(AdRequest.Builder().build())
+//            }
+//        }
+//    )
+//}
+
+
+// For preview in android studio
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    DemoAppTheme {
+//        Greeting("Android")
+//    }
+//}
+
+
---
+++ ProjectTemplets/ComposeAdmob/DemoApp/app/src/main/java/gurjar/singh/komal/demoapp/ads/GoogleMobileAdsConsentManager.kt
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package gurjar.singh.komal.demoapp.ads
+
+import android.app.Activity
+import android.content.Context
+import com.google.android.ump.ConsentForm.OnConsentFormDismissedListener
+import com.google.android.ump.ConsentInformation
+import com.google.android.ump.ConsentRequestParameters
+import com.google.android.ump.FormError
+import com.google.android.ump.UserMessagingPlatform
+
+
+
+//Testing only, remove in production
+//import com.google.android.ump.ConsentDebugSettings
+
+/**
+ * The Google Mobile Ads SDK provides the User Messaging Platform (Google's IAB Certified consent
+ * management platform) as one solution to capture consent for users in GDPR impacted countries.
+ * This is an example and you can choose another consent management platform to capture consent.
+ */
+class GoogleMobileAdsConsentManager private constructor(context: Context) {
+  private val consentInformation: ConsentInformation =
+    UserMessagingPlatform.getConsentInformation(context)
+
+  /** Interface definition for a callback to be invoked when consent gathering is complete. */
+  fun interface OnConsentGatheringCompleteListener {
+    fun consentGatheringComplete(error: FormError?)
+  }
+
+  /** Helper variable to determine if the app can request ads. */
+  val canRequestAds: Boolean
+    get() = consentInformation.canRequestAds()
+
+  /** Helper variable to determine if the privacy options form is required. */
+  val isPrivacyOptionsRequired: Boolean
+    get() =
+      consentInformation.privacyOptionsRequirementStatus ==
+        ConsentInformation.PrivacyOptionsRequirementStatus.REQUIRED
+
+  /**
+   * Helper method to call the UMP SDK methods to request consent information and load/show a
+   * consent form if necessary.
+   */
+  fun gatherConsent(
+    activity: Activity,
+    onConsentGatheringCompleteListener: OnConsentGatheringCompleteListener
+  ) {
+    // For testing purposes, remove in production
+  //  val debugSettings =
+     // ConsentDebugSettings.Builder(activity)
+     //   .setDebugGeography(ConsentDebugSettings.DebugGeography.DEBUG_GEOGRAPHY_EEA)
+        // Check your logcat output for the hashed device ID e.g.
+        // "Use new ConsentDebugSettings.Builder().addTestDeviceHashedId("ABCDEF012345")" to use
+        // the debug functionality.
+      //  .addTestDeviceHashedId("TEST-DEVICE-HASHED-ID")
+     //   .build()
+
+    val params = ConsentRequestParameters.Builder()
+        // Testing only, remove in production
+      //  .setConsentDebugSettings(debugSettings) 
+        .build()
+
+    // Requesting an update to consent information should be called on every app launch.
+    consentInformation.requestConsentInfoUpdate(
+      activity,
+      params,
+      {
+        UserMessagingPlatform.loadAndShowConsentFormIfRequired(activity) { formError ->
+          // Consent has been gathered.
+          onConsentGatheringCompleteListener.consentGatheringComplete(formError)
+        }
+      },
+      { requestConsentError ->
+        onConsentGatheringCompleteListener.consentGatheringComplete(requestConsentError)
+      }
+    )
+  }
+
+  /** Helper method to call the UMP SDK method to show the privacy options form. */
+  fun showPrivacyOptionsForm(
+    activity: Activity,
+    onConsentFormDismissedListener: OnConsentFormDismissedListener
+  ) {
+    UserMessagingPlatform.showPrivacyOptionsForm(activity, onConsentFormDismissedListener)
+  }
+
+  companion object {
+    @Volatile private var instance: GoogleMobileAdsConsentManager? = null
+
+    fun getInstance(context: Context) =
+      instance
+        ?: synchronized(this) {
+          instance ?: GoogleMobileAdsConsentManager(context).also { instance = it }
+        }
+  }
+}
